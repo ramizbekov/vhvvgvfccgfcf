1555ec0d83e3ea10185a364997bf3f199f384263
ramizbekov <jorabek_002@icloud.com>
2025-03-13 10:19:13 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+console.log('Gulchapchap')
+
+var name = "alex"
+console.log(name)
+
+let age = 25;
+age = 30;
+console.log(age);
+
+const pi = 3.14;
+
+let name1 = prompt(" Ismingizi kiritin")
+console.log("name :", name1)
+
+let name2 = prompt("Famileizi kiritin")
+console.log(name2)
+
+let name3 = prompt("Yoshingizni kiritin ")
+console.log(name3)
+
+let name4 = prompt(" Yoqtirgan fonizi kiritin")
+console.log(name4)
+
+let name5 = prompt(" Yashidigan tumanizi kiritin")
+console.log(name4)
